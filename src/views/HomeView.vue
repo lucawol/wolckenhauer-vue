724c4a9f85f89ae2f31a4b7850d8895390b6692c
CruZer0 <luca-wolckenhauer@outlook.de>
2022-03-20 16:13:10 +0100

    added software + framework skills and created the section project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,75 +11,138 @@
         <div class="introductionSection">
             <img class="introductionImage" src="../assets/lucawolckenhauer-cropped.jpg">
             <div class="introductionText">
-            <h1>hello im Luca, let me introduce myself!</h1>
-            <p>I'm really fascinated by and into technology in general. I like to understand<br>
-            how stuff works under the hood and then make something new based on this knowledge.<br>
-            In my freetime i like to do stuff with my friends and watch or read thought provoking<br>
-            articles and movies. I also do experiments on my raspberry pi like a live temperature website (solarflare-pi)
-            or hosting a nextcloud instance via docker (<a target="_blank" href="https://cloud.wolckenhauer.net">link</a>)
-            </p>
+                <h1>hello im Luca, let me introduce myself!</h1>
+                <p>I'm really fascinated by and into technology in general. I like to understand<br>
+                how stuff works under the hood and then make something new based on this knowledge.<br>
+                In my freetime i like to do stuff with my friends and watch or read thought provoking<br>
+                articles and movies. I also do experiments on my raspberry pi like a live temperature website (solarflare-pi)
+                or hosting a nextcloud instance via docker (<a target="_blank" href="https://cloud.wolckenhauer.net">link</a>)
+                </p>
             </div>
         </div>
         <div class="skillSection">
             <h1>My Skills</h1>
-                <div class="skillContainer">
-                    <div class="skilltree">
-                        <h1>languages</h1>
-                        <div class="skill">
-                            <img src="../assets/cpp-icon.png">
-                            <p>C++</p>
-                        </div>
-                        <div class="skill">
-                            <img src="../assets/python-icon.png">
-                            <p>Python</p>
-                        </div>
-                        <div class="skill">
-                            <img src="../assets/javascript-icon.png">
-                            <p>JavaScript</p>
-                        </div>
-                        <div class="skill">
-                            <img src="../assets/typescript-icon.png">
-                            <p>TypeScript</p>
-                        </div>
-                        <div class="skill">
-                            <img src="../assets/html5-icon.png">
-                            <p>HTML</p>
-                        </div>
-                        <div class="skill">
-                            <img src="../assets/css-icon.png">
-                            <p>CSS</p>
-                        </div>
+            <div class="skillContainer">
+                <div class="skilltree">
+                    <h1>languages</h1>
+                    <div class="skill">
+                        <img src="../assets/cpp-icon.png">
+                        <p>C++</p>
                     </div>
-                    <div class="skilltree">
-                        <h1>software</h1>
-                        <div class="skill">
-                            <img src="../assets/linux-icon.png">
-                            <p>Linux</p>
-                        </div>
-                        <div class="skill">
-                            <img src="../assets/windows-icon.png">
-                            <p>Windows</p>
-                        </div>
-                        <div class="skill">
-                            <img src="../assets/javascript-icon.png">
-                            <p>Microsoft Office</p>
-                        </div>
-                        <div class="skill">
-                            <img src="../assets/typescript-icon.png">
-                            <p>VMWare</p>
-                        </div>
-                        <div class="skill">
-                            <img src="../assets/html5-icon.png">
-                            <p>Docker</p>
-                        </div>
-                        <div class="skill">
-                            <img src="../assets/css-icon.png">
-                            <p>VS Code</p>
-                        </div>
+                    <div class="skill">
+                        <img src="../assets/python-icon.png">
+                        <p>Python</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/javascript-icon.png">
+                        <p>JavaScript</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/typescript-icon.png">
+                        <p>TypeScript</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/html5-icon.png">
+                        <p>HTML</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/css-icon.png">
+                        <p>CSS</p>
                     </div>
                 </div>
+                <div class="skilltree">
+                    <h1>software</h1>
+                    <div class="skill">
+                        <img src="../assets/linux-icon.png">
+                        <p>Linux</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/windows-icon.png">
+                        <p>Windows</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/msoffice-icon.png">
+                        <p>Microsoft Office</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/vmware-icon.png">
+                        <p>VMWare</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/docker-icon.png">
+                        <p>Docker</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/vscode-icon.png">
+                        <p>VS Code</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/git-icon.png">
+                        <p>Git</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/cisco-icon.png">
+                        <p>Cisco IOS</p>
+                    </div>
+                </div>
+                <div class="skilltree">
+                    <h1>frameworks</h1>
+                    <div class="skill">
+                        <img src="../assets/vuejs-icon.png">
+                        <p>VueJS</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/electron-icon.png">
+                        <p>Electron</p>
+                    </div>
+                    <div class="skill">
+                        <img src="../assets/express-icon.png">
+                        <p>ExpressJS</p>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+        <div class="projectSection">
+            <h1>Projects</h1>
+            <div class="projectContainer">
+                <div class="project">
+                    <div class="project-title">
+                        <h1>solarflare-pi</h1>
+                    </div>
+                    <div class="project-content">
+                        <p>tests with websockets and temperature readers</p>
+                        This little project was just a test
+                    </div>
+                </div>
+                <div class="project">
+                    <div class="project-title">
+                        <h1>peddit</h1>
+                    </div>
+                    <div class="project-content">
+                        <p>a electron reddit client</p>
+                        <img src="../assets/peddit-presentation.gif">
+                        <p>
+                        This App is supposed to run on a small (max. 12 inch) Screen and
+                        Scroll your personal Reddit Home Feed. 
+                        For the authentification and API Calls there is an seperate 
+                        module called reddit-juice which i have written in TS.
+                        Right now the App is in early development and far behind the feature set
+                        it should have. Later i want to add a settings menu and add touch gestures
+                        for navigation.
+                        </p>
+                    </div>
+                </div>
+                <div class="project">
+                    <div class="project-title">
+                        <h1>wolckenhauerNET</h1>
+                    </div>
+                    <div class="project-content">
+                        <p>Personal Website</p>
+                    </div>
+                </div>
+            </div>
+        </div>   
+    </div>   
 </template>
 
 <script>
@@ -89,9 +152,6 @@ export default {
 </script>
 
 <style scoped>
-  .titleSection {
-    
-  }
 
   .titleSection{
     text-align: center;
@@ -113,6 +173,9 @@ export default {
     border: white solid 0.3em;
     border-radius: 1em;
     padding: 1em;
+    animation-name: scaleDown;
+    animation-duration: 1s;
+    overflow:hidden;
   }
   .introductionSection h1{
     padding: 0;
@@ -144,8 +207,47 @@ export default {
       font-size: 1em;
       margin-bottom: .5em;
   }
+  .skill:hover{
+      transform: scale(110%);
+      cursor: pointer;
+  }
   .skillContainer{
       display: flex;
       flex-direction: row;
+  }
+  .projectContainer{
+      display: flex;
+      flex-direction:column;
+      margin-top: 1em;
+  }
+  .project{
+        margin: auto;
+      border: white solid 0.3em;
+      border-radius: 1em;
+      margin-bottom: 1em;
+      flex: 1;
+      width: 50%;
+      align-items: center;
+  }
+  .project-title{
+      background-color: white;
+      color: black;
+      
+  }
+  .project-title h1{
+      margin: 0;
+      padding: 0em 0.5em 0em 0.5em;
+      font-weight: normal;
+      text-align: center;
+  }
+  .project-content{
+      padding: 0.3em 0.2em 0.3em 0.2em;
+  }
+  .project-content img{
+      max-width: 100%;
+  }
+  @keyframes scaleDown {
+      from{scale: 400%}
+      to{scale: 100%}
   }
 </style>
